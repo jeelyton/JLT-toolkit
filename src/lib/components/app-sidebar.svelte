6682d4e0cd2a7f6f4c3c8e3d0128a79bc5e05e9a
Brook <uedsky@gmail.com>
2025-07-11 11:10:56 +0800

    feat: sidebar

---
+++ src/lib/components/app-sidebar.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+
+	import {IS_DEV} from "$lib/apis/api";
+	import { Home, Settings, Users, FileText, LogOut, Truck, ChevronUp } from "@lucide/svelte";
+    import { removeAuthToken } from "$lib/helpers/auth";
+    import { goto } from "$app/navigation";
+    import { page } from "$app/state";
+
+
+	async function handleLogout() {
+		removeAuthToken();
+		await goto('/login');
+	}
+   
+
+	const navItems = [
+		{ href: '/', label: '首页', icon: Home },
+		{ href: '/flows/delivery-notice', label: '发货(出库)通知', icon: Truck },
+		{ href: '/flows/extract-customer-po', label: '客户采购单转 Excel' },
+		{ href: '/settings', label: '设置', icon: Settings },
+	].concat(IS_DEV ? { href: '/flows/test-file-queue', label: '测试文件队列' } : []);
+
+    function isActive(href: string) {
+        return page.url.pathname === href;
+    }
+   
+</script>
+   
+<Sidebar.Root collapsible="icon">
+    <Sidebar.Header class="relative">
+        <Sidebar.Menu>
+            <Sidebar.MenuItem>
+                <Sidebar.MenuButton>
+                    <span>吉利通</span>
+                </Sidebar.MenuButton>
+            </Sidebar.MenuItem>
+        </Sidebar.Menu>
+        <Sidebar.Trigger class="absolute -right-10" />
+    </Sidebar.Header>
+    <Sidebar.Separator />
+    <Sidebar.Content>
+        <Sidebar.Group>
+        <Sidebar.GroupContent>
+            <Sidebar.Menu>
+            {#each navItems as item (item.label)}
+                <Sidebar.MenuItem>
+                <Sidebar.MenuButton isActive={isActive(item.href)}>
+                    {#snippet child({ props })}
+                    <a href={item.href} {...props}>
+                    {#if item.icon}
+                        <item.icon />
+                    {/if}
+                        <span>{item.label}</span>
+                    </a>
+                    {/snippet}
+                </Sidebar.MenuButton>
+                </Sidebar.MenuItem>
+            {/each}
+            </Sidebar.Menu>
+        </Sidebar.GroupContent>
+        </Sidebar.Group>
+    </Sidebar.Content>
+    <Sidebar.Footer><Sidebar.Menu>
+        <Sidebar.MenuItem>
+            <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+                {#snippet child({ props })}
+                <Sidebar.MenuButton
+                    {...props}
+                    class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                    Username
+                    <ChevronUp class="ml-auto" />
+                </Sidebar.MenuButton>
+                {/snippet}
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content
+                side="top"
+                class="w-(--bits-dropdown-menu-anchor-width)"
+            >
+                <DropdownMenu.Item onclick={handleLogout}>
+                <LogOut />
+                <span>退出</span>
+                </DropdownMenu.Item>
+            </DropdownMenu.Content>
+            </DropdownMenu.Root>
+        </Sidebar.MenuItem>
+        </Sidebar.Menu>
+    </Sidebar.Footer>
+</Sidebar.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,16 @@
+
 <script lang="ts">
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+   
+	let { children } = $props();
 	import '../app.css';
-	import { Button } from "$lib/components/ui/button";
-	import { Home, Settings, Users, FileText, LogOut, Truck } from "@lucide/svelte";
 	import { Toaster } from "$lib/components/ui/sonner/index.js";
 	import { page } from '$app/state';
-	import {IS_DEV} from "$lib/apis/api";
 	import { useGlobalShortcut } from "$lib/hooks/useGlobalShortcut";
-	import { removeAuthToken, isAuthenticated } from "$lib/helpers/auth";
+	import { isAuthenticated } from "$lib/helpers/auth";
 	import { goto } from "$app/navigation";
-	
-	let { children } = $props();
 
-	const navItems = [
-		{ href: '/', label: '通用', icon: Home },
-		{ href: '/flows/delivery-notice', label: '发货(出库)通知', icon: Truck },
-		{ href: '/flows/extract-customer-po', label: '客户采购单转 Excel' },
-		{ href: '/settings', label: '设置', icon: Settings },
-	].concat(IS_DEV ? { href: '/flows/test-file-queue', label: '测试文件队列' } : []);
 
 	const isLoginPage = $derived(page.url.pathname === '/login');
 
@@ -31,48 +25,16 @@
 		}
 	});
 
-	async function handleLogout() {
-		removeAuthToken();
-		await goto('/login');
-	}
 </script>
 
-<div class="flex h-screen bg-background">
-	{#if !isLoginPage}
-		<!-- Sidebar -->
-		<aside class="w-48 border-r bg-card">
-			<div class="flex h-16 items-center border-b px-6">
-				<h1 class="text-lg font-semibold">吉利通工具箱</h1>
-			</div>
-			<nav class="space-y-1 p-1">
-				{#each navItems as item}
-					<Button
-						variant="ghost"
-						class="w-full justify-start gap-2"
-						href={item.href}
-					>
-						{#if item.icon}
-							<item.icon class="h-4 w-4" />
-						{/if}
-						{item.label}
-					</Button>
-				{/each}
-				<div class="pt-4">
-					<Button
-						variant="ghost"
-						class="w-full justify-start gap-2 text-muted-foreground"
-						onclick={handleLogout}
-					>
-						<LogOut class="h-4 w-4" />
-						退出登录
-					</Button>
-				</div>
-			</nav>
-		</aside>
+   
+<Sidebar.Provider style="--sidebar-width: 12rem">
+	{#if isAuthenticated()}
+		<AppSidebar />
 	{/if}
-
-	<main class="flex-1 overflow-auto p-5 container max-w-5xl mx-auto">
-		{@render children()}
+	<main class="p-5 container max-w-5xl mx-auto">
+		{@render children?.()}
 	</main>
-	<Toaster position="bottom-center" richColors expand={true} />
-</div>
+</Sidebar.Provider>
+
+<Toaster position="bottom-center" richColors expand={true} />
