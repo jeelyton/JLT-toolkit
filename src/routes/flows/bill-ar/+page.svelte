600b8bb7995949433ddee39ece3a3bc975475491
Brook <uedsky@gmail.com>
2025-09-01 17:38:37 +0800

    fix: 单号输入框

--- src/routes/flows/bill-ar/+page.svelte
+++ src/routes/flows/bill-ar/+page.svelte
@@ -17,7 +17,8 @@
     function formatText(text: string) {
         // remove time from chat history
         // remove non-alphanumeric characters
-        const cleanText = text.toUpperCase().replace(/\d{2}:\d{2}:\d{2}/g, '')// .replace(/[^A-Z0-9-]/g, '');
+        const cleanText = text.toUpperCase().replace(/(?:\d{4}[-/]\d{1,2}[-/]\d{1,2} )?\d{2}:\d{2}:\d{2}/g, '')
+        .replace(/财务应收|[^A-Za-z0-9-]/g, (match) => match === "财务应收" ? "财务应收" : "");
         const chunks = cleanText.split(/(?=[A-Z]{2}-)/g);
         rows = chunks.length;
         return  chunks.join('\n');
--- src/routes/flows/delivery-notice/+page.svelte
+++ src/routes/flows/delivery-notice/+page.svelte
@@ -18,7 +18,7 @@
     function formatText(text: string) {
         // remove date and time from chat history
         // remove non-alphanumeric characters
-        const cleanText = text.toUpperCase().replace(/(?:\d{4}\/\d{1,2}\/\d{1,2} )?\d{2}:\d{2}:\d{2}/g, '').replace(/[^A-Z0-9-]/g, '');
+        const cleanText = text.toUpperCase().replace(/(?:\d{4}[-/]\d{1,2}[-/]\d{1,2} )?\d{2}:\d{2}:\d{2}/g, '').replace(/[^A-Z0-9-]/g, '');
         const chunks = cleanText.split(/(?=[A-Z]{2}-)/g);
         rows = chunks.length;
         return  chunks.join('\n');
